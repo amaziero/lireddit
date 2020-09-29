32043c9c169d8bfaaa7e9b92024cc34c0b054dbd
Alison Maziero <alison_lens@hotmail.com>
2020-09-29 20:09:40 -0300

    started migrations and databse with apollo and graphql

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("hello word");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import microConfig from "./mikro-orm.config";
+
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const apoloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apoloServer.applyMiddleware({ app });
+
+  app.listen(3000, () => {
+    console.log("server started at port 3000");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
