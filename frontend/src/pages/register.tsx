ae3a9567d5342bada2c50177677535ace096f19a
Alison Maziero <alison_lens@hotmail.com>
2020-10-04 20:12:23 -0300

    register and login form

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,9 +1,56 @@
 import React from "react";
+import { Formik, Form } from "formik";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/core";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
-  return <div>Register Page</div>;
+  return (
+    <Wrapper variant="regular">
+      <Formik
+        initialValues={{ usermane: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+
+              <Button
+                mt={4}
+                type="submit"
+                isLoading={isSubmitting}
+                variantColor="teal"
+              >
+                Register
+              </Button>
+            </Box>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
 };
 
 export default Register;
