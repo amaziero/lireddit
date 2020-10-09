5eaf139293f6f2a42fe6d4a470ea22c0b664ff58
Alison Maziero <alison_lens@hotmail.com>
2020-10-09 16:07:45 -0300

    added sleep function to its own file and some more refactoring

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,7 @@
-import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
-import { createUrqlClient } from "../utils/createUrqlClient";
+import { NavBar } from "../components/NavBar";
 import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
   const [{ data }] = usePostsQuery();
